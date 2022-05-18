a3945e6c819c977f6d55f984da5d8f826afb1a65
Jake Barnes <33868990+barnesjake@users.noreply.github.com>
2022-05-18 08:35:38 +0100

    [JB][SUPP-306] test data (#14)
    * [JB] add test data in cor for journeys
    
    * [JB] add some tests for journey updates

--- cor-test-data/src/main/scala/essttp/testdata/TdJourneyEpayeBta.scala
+++ cor-test-data/src/main/scala/essttp/testdata/TdJourneyEpayeBta.scala
@@ -16,6 +16,8 @@
 
 package essttp.testdata
 
+import essttp.journey.model.ttp.EligibilityCheckResult
+
 import scala.language.reflectiveCalls
 import essttp.journey.model.{Journey, NextUrl, Origin, Origins, SjRequest, SjResponse, Stage}
 import essttp.rootmodel._
@@ -33,25 +35,71 @@ trait TdJourneyEpayeBta { dependencies: TdBase with TdEpaye =>
 
     def sjRequest = SjRequest.Epaye.Simple(
       dependencies.returnUrl,
-      dependencies.backUrl,
+      dependencies.backUrl
     )
 
     def sjResponse = SjResponse(
-      nextUrl = NextUrl(s"http://localhost:9215/set-up-a-payment-plan?traceId=${dependencies.traceId.value}"),
+      nextUrl   = NextUrl(s"http://localhost:9215/set-up-a-payment-plan?traceId=${dependencies.traceId.value}"),
       journeyId = dependencies.journeyId
     )
     def postPath: String = "/epaye/bta/journey/start"
-    def sjRequestJson: JsObject = read("testdata/epaye/bta/SjRequest.json").asJson
+    def sjRequestJson: JsObject = read("/testdata/epaye/bta/SjRequest.json").asJson
 
     def journeyAfterStarted: Journey.Epaye.AfterStarted = Journey.Epaye.AfterStarted(
-      _id = dependencies.journeyId,
-      origin = Origins.Epaye.Bta,
+      _id       = dependencies.journeyId,
+      origin    = Origins.Epaye.Bta,
       createdOn = dependencies.createdOn,
       sjRequest = sjRequest,
       sessionId = dependencies.sessionId,
-      stage = Stage.AfterStarted.Started
+      stage     = Stage.AfterStarted.Started
     )
 
-    def journeyAfterStartedJson: JsObject =  read("testdata/epaye/bta/JourneyAfterStarted.json").asJson
+    def journeyAfterStartedJson: JsObject = read("/testdata/epaye/bta/JourneyAfterStarted.json").asJson
+
+    def updateTaxIdRequest(): TaxId = empRef
+
+    def updateTaxIdRequestJson(): JsObject = read("/testdata/epaye/bta/UpdateTaxIdRequest.json").asJson
+
+    def journeyAfterDetermineTaxIds: Journey.Epaye.AfterComputedTaxIds = Journey.Epaye.AfterComputedTaxIds(
+      _id       = dependencies.journeyId,
+      origin    = Origins.Epaye.Bta,
+      createdOn = dependencies.createdOn,
+      sjRequest = sjRequest,
+      sessionId = dependencies.sessionId,
+      stage     = Stage.AfterComputedTaxId.ComputedTaxId,
+      taxId     = empRef
+    )
+
+    def journeyAfterDetermineTaxIdsJson: JsObject = read("testdata/epaye/bta/JourneyAfterComputedTaxIds.json").asJson
+
+    def updateEligibilityCheckRequest(): EligibilityCheckResult = eligibleEligibilityCheckResult
+
+    def updateEligibilityCheckRequestJson(): JsObject = read("/testdata/epaye/bta/UpdateEligibilityCheckRequest.json").asJson
+
+    def journeyAfterEligibilityCheckEligible: Journey.Epaye.AfterEligibilityCheck = Journey.Epaye.AfterEligibilityCheck(
+      _id       = dependencies.journeyId,
+      origin    = Origins.Epaye.Bta,
+      createdOn = dependencies.createdOn,
+      sjRequest = sjRequest,
+      sessionId = dependencies.sessionId,
+      stage     = Stage.AfterEligibilityCheck.Eligible,
+      taxId     = empRef,
+      eligibilityCheckResult = eligibleEligibilityCheckResult
+    )
+
+    def journeyAfterEligibilityCheckEligibleJson: JsObject = read("/testdata/epaye/bta/JourneyAfterEligibilityCheck.json").asJson
+
+    def journeyAfterEligibilityCheckNotEligible: Journey.Epaye.AfterEligibilityCheck = Journey.Epaye.AfterEligibilityCheck(
+      _id       = dependencies.journeyId,
+      origin    = Origins.Epaye.Bta,
+      createdOn = dependencies.createdOn,
+      sjRequest = sjRequest,
+      sessionId = dependencies.sessionId,
+      stage     = Stage.AfterEligibilityCheck.Ineligible,
+      taxId     = empRef,
+      eligibilityCheckResult = ineligibleEligibilityCheckResult
+    )
+
+    def journeyAfterEligibilityCheckNotEligibleJson: JsObject = read("/testdata/epaye/bta/JourneyAfterEligibilityCheckNotEligible.json").asJson
   }
 }
