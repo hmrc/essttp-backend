76dc8df9fdacaec9b8fd6c4f9e0317cea6213d80
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2022-07-21 15:17:07 +0100

    [AC][SUPP-400] use playFormatSumCodecs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.66.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.67.0",
     "com.beachape"            %% "enumeratum"                 % "1.6.1",
     "org.julienrf"            %% "play-json-derived-codecs"   % "7.0.0", //choose carefully
     "io.scalaland"            %% "chimney"                    % "0.6.1",
