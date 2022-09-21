840d6ad540109d7678b494d86d3b1387d1b7369d
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2022-09-21 14:14:14 +0100

    factor out settings

---
+++ project/ScoverageSettings.scala
@@ -0,0 +1,14 @@
+import sbt.Def
+import scoverage.ScoverageKeys
+
+object ScoverageSettings {
+
+  lazy val scoverageSettings: Seq[Def.SettingsDefinition] =  Seq(
+    ScoverageKeys.coverageExcludedPackages := """uk\.gov\.hmrc\.BuildInfo;.*\.Routes;.*\.RoutesPrefix;.*\.Reverse[^.]*;testonly;.*essttp\.testdata""",
+    ScoverageKeys.coverageMinimumStmtTotal := 70.00,
+    ScoverageKeys.coverageExcludedFiles := "<empty>;.*javascript.*;.*models.*;.*Routes.*;.*testonly.*;.*DatesTdAll;.*JourneyLogger",
+    ScoverageKeys.coverageFailOnMinimum := true,
+    ScoverageKeys.coverageHighlighting := true
+  )
+
+}
