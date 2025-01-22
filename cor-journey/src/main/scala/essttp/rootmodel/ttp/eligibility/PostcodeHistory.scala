888d06ea74dc2daf97730ae95a3055a9e93b3058
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2025-01-22 11:25:13 +0000

    [AC][OPS-13240] addressPostCode->addressPostcode in eligibility

--- cor-journey/src/main/scala/essttp/rootmodel/ttp/eligibility/PostcodeHistory.scala
+++ cor-journey/src/main/scala/essttp/rootmodel/ttp/eligibility/PostcodeHistory.scala
@@ -21,7 +21,7 @@ import play.api.libs.json.{Format, Json}
 
 //TODO OPS-12584 - Clean this up when TTP has implemented the changes to the Eligibility API. addressPostCode and postcodeDate will be required
 final case class PostcodeHistory(
-    addressPostCode: Option[Postcode],
+    addressPostcode: Option[Postcode],
     postcodeDate:    Option[PostcodeDate]
 )
 
