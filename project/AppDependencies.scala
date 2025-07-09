6efe3652ce61da816efaf5cf2537c9389f9d7357
Kntanos <kostas@ntanos.co.uk>
2025-07-09 11:46:15 +0100

    [KN][OPS-13851] Dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.14.0"
+  val bootstrapVersion = "9.15.0"
   val hmrcMongoVersion = "2.6.0"
   val chimneyVersion = "1.8.1"
   val catsVersion = "2.13.0"
